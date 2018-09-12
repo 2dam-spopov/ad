bb63ffffdb1ceba10c00732418c100e2bf33f73a
2dam-spopov <2dam.spopov@institutoserpis.org>
2018-09-12 12:15:00 +0200

    Prueba de conexión a Base de Datos

---
+++ CMysql/CMysql/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+
+namespace CMysql
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+			MySqlConnection mySqlConnection = new MySqlConnection("Server=127.0.0.1;Database=dbprueba;UserID=root;Password=sistemas");
+			mySqlConnection.Open();
+			mySqlConnection.Close();
+		}
+    }
+    
+}
