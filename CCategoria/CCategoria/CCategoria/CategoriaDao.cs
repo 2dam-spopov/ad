368844052f9445e948cfd4a3cd450b3a2b54af0e
2dam-spopov <2dam.spopov@institutoserpis.org>
2018-09-28 11:28:09 +0200

    actualizado

--- CCategoria/CCategoria/CCategoria/CategoriaDao.cs
+++ CCategoria/CCategoria/CCategoria/CategoriaDao.cs
@@ -1,10 +1,25 @@
 ﻿using System;
-namespace CCategoria
-{
-    public class CategoriaDao
-    {
-        public CategoriaDao()
-        {
-        }
+using System.Collections.Generic;
+using System.Data;
+using Serpis.Ad;
+
+namespace CCategoria;
+
+
+    public class CategoriaDao{
+		private static string selectAll = "select id, nombre from categoria order by id";
+		public static IList<Categoria> Categorias{
+			get{
+				List<Categoria> categorias = new List<Categoria>();
+			    IDbCommand dbCommand = App.Instance.DbConnection.CreateCommand();
+			    dbCommand.CommandText = selectAll;
+			    IDataReader dataReader = dbCommand.ExecuteReader();
+	    		while (dataReader.Read()){
+				    categorias.Add(new Categoria((ulong)dataReader["id"], (string)dataReader["nombre"]));
+			    }
+			    dataReader.Close();
+			    return categorias;
+			}
+		}
     }
 }
