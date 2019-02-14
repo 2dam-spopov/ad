dda4409549566d2108a416fede900e9f00750268
2dam-spopov <2dam.spopov@ginstitutoserpis.org>
2019-02-14 23:32:33 +0100

    HSerpis

---
+++ HSerpis/src/main/java/serpis/ad/App.java
@@ -0,0 +1,12 @@
+package serpis.ad;
+
+import javax.persistence.EntityManagerFactory;
+
+public class App {
+	public static App getInstance() {
+		return null;
+	}
+	public EntityManagerFactory getEntityManagerFactory() {
+		return null;
+	}
+}
